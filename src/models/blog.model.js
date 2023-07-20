83589d6a4760e21dd923b5ffb91a6a2d0991af2a
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-07-20 14:01:33 +0530

    blog post model added

---
+++ src/models/blog.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const blog = mongoose.model("Blog", blogSchema);
+module.exports = blog;
